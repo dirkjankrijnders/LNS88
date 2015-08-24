3160ec8fd3fb986c8572f22f2b1948e0de876c0c
Dirkjan Krijnders <dirkjankrijnders@incas3.eu>
2015-08-24 20:54:36 +0200

    Added header files with EEPROM structure and data

---
+++ LNCV_data.h
@@ -0,0 +1,11 @@
+0x01,  //    0        1  0-2048  The module address 
+0, //  1        0  0-255   The address of the 1st S88 module
+0, //  2        0  0-255   The address of the 2nd S88 module
+0, //  3        0  0-255   The address of the 3rd S88 module
+0, //  4        0  0-255   The address of the 4th S88 module
+0, //  5        0  0-255   The address of the 5th S88 module
+0, //  6        0  0-255   The address of the 6th S88 module
+0, //  7        0  0-255   The address of the 7th S88 module
+0, //  8        0  0-255   The address of the 8th S88 module
+1,    //  9        1  0-7     Number of S88 modules connected to the S88 bus
+
